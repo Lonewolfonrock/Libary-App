e0a8e9acb29650bfe67f6126fd9d26314f2b5b2d
Anjesh Mainali <110655331+Lonewolfonrock@users.noreply.github.com>
2024-07-09 14:32:13 +0545

    init

---
+++ book/src/main/java/com/app/book/book/model/books.java
@@ -0,0 +1,26 @@
+package com.app.book.book.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class books {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int bookID;
+    private String bookName;
+    private String bookDescription;
+    private String bookAuthor;
+    private String bookImageUrl;
+
+}
