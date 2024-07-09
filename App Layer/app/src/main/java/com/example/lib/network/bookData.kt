341617e0c23ee473d253bfaf2460f9b185301bd4
Anjesh Mainali <110655331+Lonewolfonrock@users.noreply.github.com>
2024-07-09 14:21:45 +0545

    init

---
+++ App Layer/app/src/main/java/com/example/lib/network/bookData.kt
@@ -0,0 +1,9 @@
+package com.example.lib.network
+
+data class bookData (
+    var bookId: Int,
+    var bookName: String,
+    var bookDescription: String,
+    var bookAuthor: String,
+    var bookImageUrl: String
+)
